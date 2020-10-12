f10cd0dfc836cfe43da7123948a933e0719a9fe8
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-10-12 22:42:12 +0300

    lab27

--- src/App.js
+++ src/App.js
@@ -4,18 +4,37 @@ import React from 'react';
 
 import Header from './components/header/header';
 import Form from './components/form/form';
+import Results from './components/results/results'
 import Footer from './components/footer/footer';
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0, results: [] };
+  }
+  handlerForm = (headers, results) => {
+    const result = { headers, response: results };
+    this.setState({ count: results.count, results: result });
+  }
 
-function App() {
-  return (
-    <>
-    <Header />
-    <Form />
-    <Footer />
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <Form prompt='GO!' handler={this.handlerForm} />
+          <Results response={this.state.results} headers={this.state.results.headers} />
+        </main>
+        <Footer />
+
+      </>
+    )
+  }
+
+
 }
+
+
 
 export default App;
---
+++ src/components/results/results.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactJson from 'react-json-view';
+
+import './results.scss';
+
+function Results (props){
+    return(
+        <div id="resultDiv">
+            <section className="results">
+                <span>Headers</span>
+                <ReactJson id="headersID" src={props.headers} theme="shapeshifter:inverted" />
+                <span>Response</span>
+                <ReactJson id="responseID" src={props.response} theme="shapeshifter:inverted" />
+            </section>
+        </div>
+    );
+}
+
+export default Results;
