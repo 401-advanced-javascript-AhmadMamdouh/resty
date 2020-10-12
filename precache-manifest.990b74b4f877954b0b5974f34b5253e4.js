ef57d56266cd22277dfc669eb0ad6085a4ac1615
ahmadmamdouh1995 <ahmadmamdouh1995@users.noreply.github.com>
2020-10-12 13:44:40 +0000

    deploy: 806baa1e72d88bf8285f14e4034160b6393d203d

---
+++ precache-manifest.990b74b4f877954b0b5974f34b5253e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0700333c5473f4958b0dc2f4fb3ae54",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "6057a6246756d3e0169c",
+    "url": "/resty/static/css/main.84a962e0.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6057a6246756d3e0169c",
+    "url": "/resty/static/js/main.19f6e929.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
